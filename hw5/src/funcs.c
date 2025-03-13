e5e8bd2d783611d4509570f25710e48487afdbb4
Raul420 <smoke420weed@mail.ru>
2025-03-13 03:25:54 +0300

    13 night

---
+++ hw5/src/funcs.c
@@ -0,0 +1,9 @@
+#include "mylib.h"
+
+int mindiv(){
+	int res;
+	for(int i=200;(i%17)>0;i++){
+	res = i;
+	}
+	return res;
+}
---
+++ hw5/src/main.c
@@ -0,0 +1,6 @@
+#include "mylib.h"
+
+int main(){
+	printf("Минимальноче число от 200 делимое на 17 равно: %d\n", mindiv());
+	return 0;
+}

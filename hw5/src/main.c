e40042a86c05770ec87cefb2cced0e2f3354411b
Raul420 <smoke420weed@mail.ru>
2025-03-15 03:40:32 +0300

    minor changes

--- hw5/src/main.c
+++ hw5/src/main.c
@@ -1,17 +1,16 @@
 #include "mylib.h"
 
 int main(){
-	//printf("Минимальноче число от 200 делимое на 17 равно: %d\n", mindiv());
-	//int res;
-	//printf("Введите число факториала : \n");
-	//scanf("%d", &res);
-	//printf("Ответ равен : %d\n", defact(res));
-	//~ int n;
-	//~ printf("Введите количество строк: ");
-	//~ ENTER
-	//~ scanf("%d", &n);
-	//~ drawtri(n);
-	//~ return 0;
+	printf("Минимальноче число от 200 делимое на 17 равно: %d\n", mindiv());
+	int res;
+	printf("Введите число факториала : \n");
+	scanf("%d", &res);
+	printf("Ответ равен : %d\n", defact(res));
+	int n;
+	printf("Введите количество строк: ");
+	ENTER
+	scanf("%d", &n);
+	drawtri(n);
 	int a,b;
 	printf("Введите a и b: ");
 	ENTER
@@ -20,4 +19,5 @@ int main(){
 	int sec = SEC_IN_YEAR;
 	printf("секунд в году: %d", sec);
 	ENTER
+	return 0;
 }
